d73a9f2ec0fdff5e57215fa30a8b3da4c3e2fef8
hareish <hareish3@gmail.com>
2021-06-18 17:16:59 +0200

    fix bug ads

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -36,30 +36,35 @@ def create
   end
 
   def update
-   if @ad.update(ad_params)
-    redirect_to ad_path, notice:"Votre mission a été mise à jour !"
+    @ad = Ad.find(params[:id])
+ 
+
+    @ad.update(
+      date: params[:date],
+      title: params[:title],
+      adress: params[:adress],
+      description: params[:description],
+      zip_code: params[:zip_code],
+      phone: params[:phone]
+    )
+
+    if @ad.save
+        redirect_to ad_path(@ad.id), notice: "votre annonce a été bien modifierr"
+      else
+        render :new,  notice: "Erreur lors de la création de votre mission"
+      end
+  end
+
+  def destroy
+    @ad = Ad.find(params[:id])
+    if @ad.destroy
+      redirect_to ad_path, notice: "Suppression de l'événement effectuée"
     else
+      flash.now[:notice] = "L'événement n'a pas pu être supprimé"
       render :edit
     end
   end
-    
- def destroy
-    @ad.destroy
-    redirect_to ad_path, notice:"Votre mission a été supprimée !"
-  end
-
 
   private
-
-  def ad_params
-    params.require(:ad).permit(
-      :title,
-      :phone,
-      :description,
-      :date,
-      :zip_code,
-     )
-  end
-
   
 end
