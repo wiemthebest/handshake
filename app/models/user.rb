c11fee49673f3f700546b783090b65071f4d55e4
hareish <hareish3@gmail.com>
2021-06-18 19:05:37 +0200

    fix bug ads

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,5 @@ class User < ApplicationRecord
 
 
 
-  validates :encrypted_password,
-            presence: true,
-            length: { minimum: 6 }
-
-    validates :email,
-            presence: true,
-            uniqueness: true,
-            format: {
-              with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/,
-              message: 'email adress please'
-            }
-     
-  has_many :participations
-  has_many :ads, through: :participations
-
-
 
 end
