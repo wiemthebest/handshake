a617afd13c59938a67d44d08c4b4a9a3f2092709
Wiem Trabelsi <wiem2007@gmail.com>
2021-06-24 23:37:02 +0200

    fix styles

--- db/schema.rb
+++ db/schema.rb
@@ -125,8 +125,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_233623) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "users", column: "receiver_id"
   add_foreign_key "messages", "users", column: "sender_id"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
 end
