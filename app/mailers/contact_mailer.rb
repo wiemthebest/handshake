cbaccbe5e10f56b7fe35a384a43ca0306fc6bf7f
Wiem Trabelsi <wiem2007@gmail.com>
2021-06-28 12:24:42 +0200

    change contact from

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,7 @@ class ContactMailer < ApplicationMailer
     def contact_form(contact)
       @contact = contact
       admins_emails = User.admin.pluck(:email)
-      mail(from: contact.email, to: 'wiem2007@gmail.com', subject: "Contact from #{contact.first_name} #{contact.last_name}")
+      mail(to: 'wiem2007@gmail.com', subject: "Contact from #{contact.first_name} #{contact.last_name}")
     end
 
 end
